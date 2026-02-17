fa3a8d17109f1e13d5870941ba4bc7e591024067
Robin <singhr@roverkids.org>
2026-02-17 11:18:38 -0500

    Radar1
    kinda working radar for the most part

--- StarScream/StarScream.cs
+++ StarScream/StarScream.cs
@@ -1,5 +1,8 @@
+
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
+using System;
+using System.Xml.Linq;
 
 // ------------------------------------------------------------------
 // MyFirstBot
@@ -23,16 +26,57 @@ public class StarScream : Bot
         // Repeat while the bot is running
         while (IsRunning)
         {
-            Forward(100);
-            TurnGunLeft(360);
-            Back(100);
-            TurnGunLeft(360);
+            //Forward(100);
+            TurnRadarLeft(10);
+           
+           // Back(100);
+            //TurnGunLeft(360);
         }
     }
 
     // We saw another bot -> fire!
     public override void OnScannedBot(ScannedBotEvent evt)
     {
+        double x1 = evt.X;
+        double y1 = evt.Y;
+        double x2 = X;
+        double y2 = Y;
+        double xt = x2 - x1;
+        double yt = y2 - y1;
+        double angle = Math.Atan2(Math.Abs(xt), Math.Abs(yt));
+        angle = angle * 180 / Math.PI;
+        double RelativeAngle = 0;
+        if (yt>0)
+        {
+            if (xt > 0)
+            {
+                RelativeAngle = 270 - angle;
+            }
+            if (xt < 0)
+            {
+                RelativeAngle = 270 + angle;
+            }
+        }
+        if (yt < 0)
+        {
+            if (xt > 0)
+            {
+                RelativeAngle = 90 + angle;
+            }
+            if (xt < 0)
+            {
+                RelativeAngle = 90 - angle;
+            }
+        }
+        double turnAngle = RelativeAngle - RadarDirection;
+       if (turnAngle > 0)
+        {
+            TurnRadarRight(turnAngle);
+        }
+       if (turnAngle < 0)
+        {
+            TurnRadarLeft(turnAngle);
+        }
         Fire(1);
     }
 
@@ -40,9 +84,9 @@ public class StarScream : Bot
     public override void OnHitByBullet(HitByBulletEvent evt)
     {
         // Calculate the bearing to the direction of the bullet
-        var bearing = CalcBearing(evt.Bullet.Direction);
+        //var bearing = CalcBearing(evt.Bullet.Direction);
 
         // Turn 90 degrees to the bullet direction based on the bearing
-        TurnRight(90 - bearing);
+        //TurnRight(90 - bearing);
     }
 }
